58596421e45a3d1306df0d6b24c43b05876a3b2e
BED-COM-26-20 <bed-com-26-20@unima.ac.mw>
2026-04-06 22:33:54 +0200

    dashboard routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+// import { useState } from 'react';
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// import { Dash }       from './dashboard/Dash';
+// import { SetupPage }  from './dashboard/SetupPage';
+// import { LessonPage } from './dashboard/LessonPage';
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/setup"                  element={<SetupPage />} />
+//         <Route path="/"                       element={<Dash />} />
+//         <Route path="/lesson/:subject/:topic" element={<LessonPage />} />
+//         <Route path="*"                       element={<Navigate to="/" replace />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
-import Chat from './pages/Chat';
+
+import { Dash }       from './dashboard/Dash';
+import { SetupPage }  from './dashboard/SetupPage';
+import { LessonPage } from './dashboard/LessonPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/"                       element={<Home />} />
+        <Route path="/login"                  element={<Login />} />
+        <Route path="/register"               element={<Register />} />
+        <Route path="/forgot-password"        element={<ForgotPassword />} />
+       
+        <Route path="/dash"              element={<Dash />} />
+        <Route path="/setup"                  element={<SetupPage />} />
+        <Route path="/lesson/:subject/:topic" element={<LessonPage />} />
+      
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
