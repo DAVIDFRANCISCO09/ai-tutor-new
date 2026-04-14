50715f4c2e333ae083bfea636b7e9152ca473f3f
ISSA NIPULO <BED/COM/01/20@COMLAB.COM>
2026-04-14 16:27:52 +0200

    updating the quiz page flow and dependecies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import ForgotPassword from './pages/ForgotPassword';
-import Chat from './pages/Chat';
+import QuizPage from "./Quiz/QuizPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <QuizPage />;
 }
 
 export default App;
