3c25c6dbd0beba68ea52da07881a36e868a4acfd
BED-COM-26-20 <bed-com-26-20@unima.ac.mw>
2026-05-12 14:54:15 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import Chat from './pages/Chat';
-import History from './pages/History';
+//import History from './pages/History';
 import ProgressPage from './components/myprogress/progresstracking';
 import Dash from './dashboard/Dash';               
-import SetupPage from './dashboard/SetupPage';      
+//import SetupPage from './dashboard/SetupPage';      
 import LessonPage from './dashboard/LessonPage';    
 import { Toaster } from 'react-hot-toast';
 import QuizPage from './Quiz/QuizPage';
@@ -29,7 +29,7 @@ function App() {
         <Route path="/lesson/:subject/:topic" element={<LessonPage />} />
         <Route path="/quiz/:subject/:topic" element={<QuizPage />} />
         <Route path="/chat" element={<Chat />} />
-        <Route path="/history" element={<Navigate to="/chat" replace />} />
+        {/* <Route path="/history" element={<Navigate to="/chat" replace />} /> */}
         <Route path="/progress" element={<ProgressPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
