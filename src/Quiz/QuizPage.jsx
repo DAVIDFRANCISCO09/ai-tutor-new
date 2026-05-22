11b03120ee0378a30fedf053e4477308223198fd
Issah Nipulo <bed-com-01-20@unima.ac.mw>
2026-05-22 12:24:28 +0200

    updating code

--- src/Quiz/QuizPage.jsx
+++ src/Quiz/QuizPage.jsx
@@ -130,10 +130,7 @@ export default function QuizPage() {
     toast.success(`Subject "${subject}" completed!! Choose another subject to continue learning.`);
   };
 
-  const handleLogout = () => {
-    localStorage.clear();
-    navigate('/');
-  };
+
 
   if (loading) {
     return (
